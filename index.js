c6cefea493542555b62fa1ecfcba74006ac1c76b
Conrad Etherington <conrad.etherington@gmail.com>
2022-09-24 13:14:39 -0600

    commit tree 05ccdbb4e78f3b6422c369cf71aa8aa5f4ffbbc5

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.splice(-1)
+}
+function destructivelyRemoveFirstCat() {
+    cats.splice(0,1)
+}
+function appendCat() {
+    const copyCats = [...cats, 'Broom']
+    return copyCats
+}
+function prependCat() {
+    const copyCats = ['Arnold', ...cats]
+    return copyCats
+}
+function removeLastCat() {
+    const copyCats = [...cats.slice(0,-1)]
+    return copyCats
+}
+function removeFirstCat() {
+    const copyCats = [...cats.slice(1)]
+    return copyCats
+}
